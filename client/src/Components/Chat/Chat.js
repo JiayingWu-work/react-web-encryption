be1befe580c37ba29aa622f2fa5e1b1e30d0498d
Ned Mayo <emayo@macalester.edu>
2022-12-13 01:16:09 -0600

    hi

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -7,7 +7,7 @@ import Input from "../Input/Input";
 import { encryptMessage } from "../../Encryption/index.js";
 import "./chat.css";
 
-const ENDPOINT = "https://radiant-temple-08988.herokuapp.com/";
+const ENDPOINT = "https://sample.herokuapp.com/";
 
 let socket;
 
